04dc33cb78216b324f6b631bb3ecae165c3aa370
yxn343 <546409599@qq.com>
2023-04-08 16:23:06 +0800

    代码在每一次提交的时候 都需要提供提交信息 如果是多人协同项目 你需要通过提交信息告诉别人 你提交了什么代码 实现了什么功能 或者解决了哪些问题

---
+++ src/main/java/com/iweb/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.iweb.test;
+
+/**
+ * @author Yang
+ * @date 2023/4/8 16:12
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("343全员ikun");
+    }
+}
